885efaf41a9f87136953886fd3992ed25a10a075
Brian Kim <bk@elevateclub.org>
2024-03-08 11:08:39 -0500

    align with karpathy script

--- train.py
+++ train.py
@@ -1,15 +1,26 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+# Hyperparamaters
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'mps' 
+eval_iters = 200
+
 torch.manual_seed(1337)
 
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # Establish tokenization vocabulary
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-
+# Create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s]
@@ -20,9 +31,6 @@ data = torch.tensor(encode(text), dtype=torch.long) # TT: decode text in tensor 
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
-
-batch_size = 4 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
 
 """ example of how targets are set with batch and block size
 x = train_data[:block_size]
@@ -42,6 +50,7 @@ def get_batch(split):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x, y 
 
+""" example to explain how inputs and targets
 xb, yb = get_batch("train")
 print("inputs: ", xb.shape, xb)
 print("targets: ", yb.shape, yb)
@@ -52,14 +61,17 @@ for b in range(batch_size):
         context = xb[b, :t+1]
         target = yb[b, t]
         print(f"when input is {context.tolist()} the target: {target}")
+"""
 
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
     
     def forward(self, idx, targets=None):
-        logits = self.token_embedding_table(idx)
+        # idx and targets are both (B, T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
 
         if targets is None:
             loss = None
@@ -87,17 +99,16 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 m = BigramLanguageModel(vocab_size)
+""" example of running model once
 logits, loss = m(xb, yb)
 print(logits.shape)
 print(loss)
-
-
+"""
 
 # Create a pytorch optimizer
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
-batch_size = 32
-for steps in range(10000):
+for steps in range(max_iters):
     # sample a batch of data
     xb, yb = get_batch('train')
 
@@ -105,10 +116,9 @@ for steps in range(10000):
     logits, loss = m(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
-    
     optimizer.step()
 
-print(loss.item())
-
-print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=400)[0].tolist()))
+# print(loss.item())
+ctx = torch.zeros((1, 1), dtype=torch.long)
+print(decode(m.generate(ctx, max_new_tokens=500)[0].tolist()))
 
