cc42b2a88ce6a036698b01476609238f3477ea07
Brian Kim <bk@elevateclub.org>
2024-03-12 10:53:20 -0400

    micrograd

---
+++ MicroGrad/main.py
@@ -0,0 +1,24 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Value:
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+    
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
+
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.0)
+
+d = a*b+c
