1853bd1cd6d1d3713c5f19021c643f1e5471bdcf
Brian Kim <bk@elevateclub.org>
2024-03-13 00:46:02 -0400

    set up the neural net implementation of bigram

--- makemore/main.py
+++ makemore/main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
+import torch.nn.functional as F
 
 words = open('names.txt', 'r').read().splitlines()
 
@@ -64,3 +65,23 @@ print(f'{log_likelihood=}')
 nll = -log_likelihood
 print(f'{nll=}')
 print(f'{nll/n}')
+
+# create the training set of bigrams (x,y)
+xs, ys = [], []
+
+for w in words[:1]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        xs.append(ix1)
+        ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+xenc = F.one_hot(xs, num_classes=27).float()
+W = torch.rand((27, 27))
+logits = xenc @ W
+counts = logits.exp()
+probs = counts / counts.sum(1, keepdims=True)
