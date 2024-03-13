94f7081098e99a44b7ee713bed9f0c4f576b35a9
Brian Kim <bk@elevateclub.org>
2024-03-12 23:44:41 -0400

    load bigram into pytorch tensor

--- makemore/main.py
+++ makemore/main.py
@@ -1,9 +1,19 @@
+import torch
+
 words = open('names.txt', 'r').read().splitlines()
 
-b = {}
+N = torch.zeros((28, 28), dtype=torch.int32)
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i for i,s in enumerate(chars)}
+stoi['<S>'] = 26
+stoi['<E>'] = 27
+
+itos = {i:s for i,s in enumerate(chars)}
+
 for w in words:
     chs = ['<S>'] + list(w) + ['<E>']
     for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
         bigram = (ch1, ch2)
-        b[bigram] = b.get(bigram, 0) + 1
-        
