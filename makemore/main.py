ab5fcba753b879c2d27bad7bd1a98b36790bbb98
Brian Kim <bk@elevateclub.org>
2024-03-12 23:59:57 -0400

    use common start and end token

--- makemore/main.py
+++ makemore/main.py
@@ -3,16 +3,14 @@ import torch
 
 words = open('names.txt', 'r').read().splitlines()
 
-N = torch.zeros((28, 28), dtype=torch.int32)
+N = torch.zeros((27, 27), dtype=torch.int32)
 chars = sorted(list(set(''.join(words))))
-stoi = {s:i for i,s in enumerate(chars)}
-stoi['<S>'] = 26
-stoi['<E>'] = 27
-
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
 
 for w in words:
-    chs = ['<S>'] + list(w) + ['<E>']
+    chs = ['.'] + list(w) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
@@ -21,8 +19,8 @@ for w in words:
 
 plt.figure(figsize=(16,16))
 plt.imshow(N, cmap='Blues')
-for i in range(28):
-    for j in range(28):
+for i in range(27):
+    for j in range(27):
         chstr = itos[i] + itos[j]
         plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
         plt.text(j, i, N[i,j].item(), ha="center", va="top", color='gray')
