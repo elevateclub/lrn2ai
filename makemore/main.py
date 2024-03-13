1c2dcf1d06407dfbcb9a5729d5a7e6547fff815c
Brian Kim <bk@elevateclub.org>
2024-03-12 23:59:36 -0400

    plot bigram frequencies

--- makemore/main.py
+++ makemore/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch
 
 words = open('names.txt', 'r').read().splitlines()
@@ -8,7 +9,7 @@ stoi = {s:i for i,s in enumerate(chars)}
 stoi['<S>'] = 26
 stoi['<E>'] = 27
 
-itos = {i:s for i,s in enumerate(chars)}
+itos = {i:s for s,i in stoi.items()}
 
 for w in words:
     chs = ['<S>'] + list(w) + ['<E>']
@@ -17,3 +18,13 @@ for w in words:
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
         bigram = (ch1, ch2)
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(28):
+    for j in range(28):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+        plt.text(j, i, N[i,j].item(), ha="center", va="top", color='gray')
+plt.axis('off')
+plt.show()
