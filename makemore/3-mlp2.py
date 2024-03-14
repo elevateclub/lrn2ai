a2a6dcf72c1b30b24312037f700858c5df888428
Brian Kim <bk@elevateclub.org>
2024-03-14 09:50:29 -0400

    initialization: improve initial loss

--- makemore/3-mlp2.py
+++ makemore/3-mlp2.py
@@ -46,11 +46,11 @@ n_embd = 10 # dimensionality of the character embedding vectors
 n_hidden = 200 # number of neurons in the hidden layer of MLP
 
 g = torch.Generator().manual_seed(2147483647)
-C = torch.randn((vocab_size, n_embd), generator=g)
+C = torch.randn((vocab_size, n_embd),             generator=g)
 W1 = torch.randn((n_embd * block_size, n_hidden), generator=g)
-b1 = torch.randn(n_hidden, generator=g)
-W2 = torch.randn((n_hidden, vocab_size), generator=g)
-b2 = torch.randn(vocab_size, generator=g)
+b1 = torch.randn(n_hidden,                        generator=g)
+W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
+b2 = torch.randn(vocab_size,                      generator=g) * 0
 
 parameters = [C, W1, b1, W2, b2]
 print(sum(p.nelement() for p in parameters))
