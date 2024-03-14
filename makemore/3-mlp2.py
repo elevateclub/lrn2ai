d3dd53e5807cc7d1270a40ba9b180502e7126cf4
Brian Kim <bk@elevateclub.org>
2024-03-14 09:56:35 -0400

    initialization: avoid dead neurons by reducing tanh saturation

--- makemore/3-mlp2.py
+++ makemore/3-mlp2.py
@@ -47,8 +47,8 @@ n_hidden = 200 # number of neurons in the hidden layer of MLP
 
 g = torch.Generator().manual_seed(2147483647)
 C = torch.randn((vocab_size, n_embd),             generator=g)
-W1 = torch.randn((n_embd * block_size, n_hidden), generator=g)
-b1 = torch.randn(n_hidden,                        generator=g)
+W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * 0.2
+b1 = torch.randn(n_hidden,                        generator=g) * 0.01
 W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
 b2 = torch.randn(vocab_size,                      generator=g) * 0
 
