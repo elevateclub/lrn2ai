98c26943d25315c7203f14efd34d459ba3e85bd5
Brian Kim <bk@elevateclub.org>
2024-03-14 10:12:51 -0400

    init: kaiming norm

--- makemore/3-mlp2.py
+++ makemore/3-mlp2.py
@@ -47,7 +47,7 @@ n_hidden = 200 # number of neurons in the hidden layer of MLP
 
 g = torch.Generator().manual_seed(2147483647)
 C = torch.randn((vocab_size, n_embd),             generator=g)
-W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * 0.2
+W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * (5/3)/(n_embd*block_size)**0.5 # kaiming norm
 b1 = torch.randn(n_hidden,                        generator=g) * 0.01
 W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
 b2 = torch.randn(vocab_size,                      generator=g) * 0
