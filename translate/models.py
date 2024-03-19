706fcd669ea6eb9eb82081760acf3544c0833dd4
elevateclub <bk@elevateclub.org>
2024-03-18 23:21:47 -0400

    implement optimizer

--- translate/models.py
+++ translate/models.py
@@ -12,4 +12,20 @@ class ScheduledOptim():
         self.n_steps = 0
 
     def step_and_update_lr(self):
-        pass
+        self._update_learning_rate()
+        self._optimizer.step()
+
+    def zero_grad(self)
+        self._optimizer.zero_grad()
+
+    def _get_lr_scale(self):
+        d_model = self.d_model
+        n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
+        return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
+
+    def _update_learning_rate(self):
+        self.n_steps += 1
+        lr = self.lr_mul * self._get_lr_scale()
+
+        for param_group in self._optimizer.param_groups:
+            param_group['lr'] = lr
